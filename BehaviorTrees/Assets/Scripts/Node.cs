6ea20e7fec0c6e6ad637908a6c5ff24e7807d077
Ethan Heil <eheil20@gmail.com>
2020-12-01 16:01:12 -0500

    Adding abstract Node class

---
+++ BehaviorTrees/Assets/Scripts/Node.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class Node
+{
+    protected NodeState mNodeState;
+    public NodeState nodeState { get { return mNodeState; } }
+
+    public abstract NodeState Evaluate();
+}
+
+public enum NodeState
+{
+    RUNNING, SUCCESS, FAILURE
+}
