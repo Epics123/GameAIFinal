6fd853e9333f9431e1994e10d99be8fd05d03fd9
Ethan Heil <eheil20@gmail.com>
2020-12-06 18:01:55 -0500

    Adding movement to the player for easier testing

---
+++ BehaviorTrees/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 movement = new Vector3(0.0f, 0.0f, 1.0f);
+            transform.position += movement * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 movement = new Vector3(0.0f, 0.0f, -1.0f);
+            transform.position += movement * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
+            transform.position += movement * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 movement = new Vector3(-1.0f, 0.0f, 0.0f);
+            transform.position += movement * Time.deltaTime * speed;
+        }
+    }
+}
